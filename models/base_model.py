768b50c1ce9a06bb09101a050292fc7ef9f672d5
David Onwuli <chines_davi@yahoo.com>
2023-01-06 16:35:02 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 from datetime import datetime
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -10,10 +11,10 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id =collumn(str(uuid.uuid4()),primary_key=True)
+            self.created_at =collumn(datetime.utcnow())
+            self.updated_at =collumn(datetime.utcnow())
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +32,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -42,3 +44,6 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        
