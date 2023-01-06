ae420aa9b14224202d04ea9fe2c61fdb2d9464b8
David Onwuli <chines_davi@yahoo.com>
2023-01-06 17:03:46 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -7,5 +7,5 @@ class City(BaseModel,Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'City'
     
-    state_id = ""
-    name = ""
+    state_id = Column(String(128))
+    name = Column(String(60))
