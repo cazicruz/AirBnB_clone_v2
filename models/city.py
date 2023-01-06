d12e63923e5434543d506e1e896521ab472f4423
David Onwuli <chines_davi@yahoo.com>
2023-01-06 16:43:44 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -3,7 +3,9 @@
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
+class City(BaseModel,Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'City'
+    
     state_id = ""
     name = ""
