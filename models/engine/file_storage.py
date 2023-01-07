1cfb344e59a1438d7fc172675a898977ffedc028
onojaonoja2 <samuelonoja06@gmail.com>
2023-01-07 21:47:19 +0100

    updated storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,19 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+         def delete(self, obj=None):
+        """public instance method to delete obj from __objects
+        if it’s inside
+        """
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
+            self.save()
+
+    def close(self):
+        """Public method to call reload for deserializing the
+        JSON file.
+        """
+        self.reload()
